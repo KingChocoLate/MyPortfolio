2d035dbd54863d170b937955747d2280d6a62e8f
KingChocoLate <freedomearning123@gmail.com>
2025-02-22 12:09:10 +0700

    Pushing

--- tp9Script.js
+++ tp9Script.js
@@ -1,12 +1,62 @@
-function Decrease(){
-    let Value = parseInt(document.getElementById("kdmv").value);
-    Value--;
-    alert(Value);
-    document.getElementById("kdmv").innerHTML = toString(Value);
+//Ex01
+class Counter{
+    Value = 0;
+    constructor(){
+        document.getElementById("kdmv").innerHTML = this.Value;
+    };
+    Decrease(){
+        this.Value--;
+        document.getElementById("kdmv").innerHTML = this.Value;
+    }
+    Reset(){
+        this.Value = 0;
+        document.getElementById("kdmv").innerHTML = this.Value;
+    }
+    Increase(){
+        this.Value++;
+        document.getElementById("kdmv").innerHTML = this.Value;
+    }
 }
-function Reset(){
-    document.getElementById("kdmv").innerHTML = "0";
+
+const counter = new Counter();
+function Decrease(){
+    counter.Decrease();
+}
+function Reset(){  
+    counter.Reset();
 }
 function Increase(){
-    document.getElementById("kdmv").innerHTML = "KDMH";
+    counter.Increase();
+}
+
+function handleMouseOver() {
+    document.getElementById("decrease").style.backgroundColor = 'yellow';
+}
+
+function handleMouseOut() {
+    event.target.style.backgroundColor = '';
+}
+
+//Ex02
+
+class Color{
+    colorType = '';
+    records = [];
+
+    constructor(type){
+        this.colorType = type;
+    }
+
+    generate(){
+
+    }
+    getRandomNumber(){
+        
+    }
+    getRecords(){
+
+    }
+    reset(){
+        
+    }
 }
