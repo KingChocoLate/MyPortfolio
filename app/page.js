72e5142a3afdd73196d3922509fa005cb02f2202
KingChocoLate <freedomearning123@gmail.com>
2025-02-27 21:59:28 +0700

    MyPortfolio

--- app/page.js
+++ app/page.js
@@ -2,100 +2,40 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <body class="bg-gray-950 text-white">
+    <div class="mx-16 mt-5 flex flex-col items-center w-auto h-[50px] text-[10px] gap-5">
+        <div class="flex justify-between w-full opacity-25">
+            <div>freedomearning123@gmail.com</div>
+            <p>facebook / Telegram / Instagram</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div class="border border-gray-400 opacity-25 w-full"></div>
     </div>
+    <div class="mt-10 flex flex-col items-center gap-5">
+        <div class="bg-white w-20 h-20 rounded-full"></div>
+        <p class="text-[10px] text-gray-500">Hi, I'm Odom</p>
+        <div class="text-3xl animate-pulse">
+            <p class="text-center">Building digital</p>
+            <p class="text-center">products, brands, and</p>
+            <p class="text-center">experience.</p>
+        </div>
+        <button id="bttn" class="text-[8px] text-gray-300 bg-black w-[120px] h-[40px] rounded-lg">About Me</button>
+    </div>
+
+    <div class="mt-10 w-full h-[130px] bg-gray-800"></div>
+
+    <div class="mt-20 text-xl animate-pulse">
+        <p class="text-center">Collaborate with brands and agencies to</p>
+        <p class="text-center">create impactful results.</p>
+    </div>
+
+    <div class="mt-10 mx-3 flex justify-center gap-3">
+        <div class="w-[180px] h-[200px] bg-gray-800"></div>
+        <div class="w-[180px] h-[200px] bg-gray-800"></div>
+        <div class="w-[180px] h-[200px] bg-gray-800"></div>
+        <div class="w-[180px] h-[200px] bg-gray-800"></div>
+    </div>
+
+    <script src="PortfolioJs.js"></script>
+</body>
   );
 }
